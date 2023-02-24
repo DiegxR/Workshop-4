85602d094b20cde8559865d4c6629d1dd651e289
ManuelaCardona <cartagenamanuela11@gmail.com>
2023-02-23 21:11:22 -0500

    home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './style.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='texts'>
+        <p className='firstText'>SO, YOU WANT TO TRAVEL TO</p>
+        <p className='secondText'>SPACE</p>
+        <p className='thirdText'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we´ll give you a truly out of this world experience!</p>
+      </div>
+      <button className='explore'>EXPLORE</button>
+    </section>
   )
 }
 
